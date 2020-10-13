10c26d435d1784e19171cbca9d72d2e37fb28eaf
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2020-10-14 00:32:04 +0300

    Add files via upload

--- secure.sh
+++ secure.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#--Check if user infected or neither 
+cd
+touch testfile
+echo “ASDFZXCV:hf:testfile” > /dev/zero && ls
+echo "if this above returns a missing testfile file, that means you're infected(Press ANY KEY)."
+read -p '>: '
+
+#--Check for unsigned kernel modules
+for mod in $(lsmod | tail -n +2 | cut -d' ' -f1); do modinfo ${mod} | grep -q "signature" || echo "no signature for module: ${mod}" ; done
+
+
 #--Required Packages: ufw fail2ban net-tools
 sudo apt install --install-recommends ufw fail2ban mini-httpd certbot net-tools apt-transport-https -y
 
@@ -9,6 +20,7 @@ sudo ufw allow 443/tcp
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw enable
+
 
 #--Harden /etc/sysctl.conf
 printf "kernel.dmesg_restrict = 1
@@ -36,24 +48,29 @@ net.ipv6.icmp.echo_ignore_all = 1
 vm.dirty_background_bytes = 4194304
 vm.dirty_bytes = 4194304" > /etc/sysconf.conf
 
+
 #--PREVENT IP SPOOFS
 cat <<EOF > /etc/host.conf
 order bind,hosts
 multi on
 EOF
 
+
 #--Enable fail2ban
 sudo cp fail2ban.local /etc/fail2ban/
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 
+
 #--Pacify apport
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+
 
 #--Renew certificates
 sudo systemctl stop mini-httpd.service
 sudo certbot renew
 sudo systemctl start mini-httpd.service
+
 
 #--Listen current traffic
 echo "listening ports"
