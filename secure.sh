b02ce93f0008d12595b798697fe66ff1b6ce87eb
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2020-11-12 13:14:52 +0300

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -12,7 +12,7 @@ clear
 for mod in $(lsmod | tail -n +2 | cut -d' ' -f1); do modinfo ${mod} | grep -q "signature" || echo "no signature for module: ${mod}"; done
 
 #--Required Packages: ufw fail2ban net-tools
-sudo apt install --install-recommends ufw fail2ban mini-httpd certbot net-tools apt-transport-https -y
+sudo apt install --install-recommends ufw fail2ban mini-httpd certbot net-tools apt-transport-https ansible -y
 
 #--Setup UFW rules
 sudo ufw limit 22/tcp
@@ -23,7 +23,7 @@ sudo ufw default allow outgoing
 sudo ufw enable
 
 #--Harden /etc/sysctl.conf
-printf "kernel.dmesg_restrict = 1
+sudo printf "kernel.dmesg_restrict = 1
 kernel.modules_disabled=1
 kernel.kptr_restrict = 1
 net.core.bpf_jit_harden=2
@@ -49,7 +49,7 @@ vm.dirty_background_bytes = 4194304
 vm.dirty_bytes = 4194304" >/etc/sysconf.conf
 
 #--PREVENT IP SPOOFS
-cat <<EOF >/etc/host.conf
+sudo cat <<EOF >/etc/host.conf
 order bind,hosts
 multi on
 EOF
@@ -61,6 +61,46 @@ sudo systemctl start fail2ban
 
 #--Pacify apport
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+
+#--Pacify LLMNR
+sudo sed -i 's/#LLMNR=yes/LLMNR=no/g' /etc/systemd/resolved.conf
+
+#--Default umask in /etc/profile or /etc/profile.d/custom.sh could be more strict
+if [ $UID -gt 199 ] && [ "`id -gn`" = "`id -un`" ]; then
+    umask 027
+else
+    umask 027
+fi
+
+#--Configure minimum & maximum encryption algorithm rounds in /etc/login.defs
+sudo sed -i 's/# SHA_CRYPT_MIN_ROUNDS 5000/SHA_CRYPT_MIN_ROUNDS 5000/g' /etc/login.defs
+sudo sed -i 's/# SHA_CRYPT_MAX_ROUNDS 5000/SHA_CRYPT_MAX_ROUNDS 50000/g' /etc/login.defs
+
+#--Harden compilers like restricting access to root user only
+sudo chmod o-rx /usr/bin/gcc
+sudo chmod o-rx /usr/bin/as
+
+#--Copy /etc/fail2ban/jail.conf to jail.local to prevent it being changed by updates
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+#--Consider restricting file permissions
+sudo chmod og-rwx /etc/cron.*
+
+#--Double check the permissions of home directories as some might be not strict enough.
+chmod 750 /home/$USER
+
+#--Fix jail.local
+sudo cat <<EOF >/etc/fail2ban/jail.local
+[DEFAULT]
+ ignoreip = 127.0.0.1/8 ::1
+ bantime = 3600
+ findtime = 600
+ maxretry = 5
+ enabled = true
+
+[sshd]
+ enabled = true
+EOF
 
 #--Renew certificates
 sudo systemctl stop mini-httpd.service
