6e1ed4622b0a50a9b93b8364e8fc3bd929d4b847
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-08-30 20:23:00 +0000

    Edit secure.sh

--- secure.sh
+++ secure.sh
@@ -25,7 +25,8 @@
 #--Required Packages: ufw fail2ban net-tools
 which apt >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-    sudo apt install -f --assume-yes --no-install-recommends openssl ufw fail2ban net-tools unattended-upgrades proxychains ca-certificates certbot
+    sudo apt update &&
+        sudo apt install -f --assume-yes --no-install-recommends openssl ufw fail2ban net-tools unattended-upgrades proxychains ca-certificates certbot
     echo -e 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee /etc/apt/apt.conf.d/50unattended-upgrades
     echo -e 'APT::Periodic::AutocleanInterval "7";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
     echo -e 'APT::Periodic::Download-Upgradeable-Packages "1";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
@@ -37,7 +38,8 @@ if [ $? -eq 0 ]; then
 fi
 which pacman >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-    yay -S --needed --noconfirm openssl ufw fail2ban net-tools proxychains ca-certificates certbot
+    sudo pacman -Syy &&
+        yay -S --needed --noconfirm openssl ufw fail2ban net-tools proxychains ca-certificates certbot
 fi
 
 clear
