fbd072e878ccf5a2aba128b4d467fd0e44b1be79
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-09-06 09:41:40 +0300

    Edit secure.sh

--- secure.sh
+++ secure.sh
@@ -57,14 +57,11 @@ sudo ufw reload
 
 #--Harden /etc/sysctl
 #echo -e "kernel.sched_energy_aware = 1
-kernel.dmesg_restrict = 1
 kernel.modules_disabled=1
 kernel.kptr_restrict = 1
 net.core.bpf_jit_harden=2
 kernel.yama.ptrace_scope=3
 kernel.kexec_load_disabled = 1
-kernel.nmi_watchdog=0
-kernel.sysrq=0
 net.ipv4.tcp_rfc1337=1
 net.ipv4.conf.default.rp_filter=1
 net.ipv4.conf.all.rp_filter=1
