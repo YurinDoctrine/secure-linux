cf6f43b2e59c76074cdacf432b4eb3039d372ceb
Yurin <43861005+YurinDoctrine@users.noreply.github.com>
2022-01-17 23:42:57 +0300

    edit secure.sh

--- secure.sh
+++ secure.sh
@@ -2,7 +2,7 @@
 
 # MIT License
 
-# Copyright (c) 2020-2022 Yurin
+# Copyright (c) 2020-2022 Yurin Doctrine
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
