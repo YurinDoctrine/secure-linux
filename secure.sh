2f92e63d775c3e4c20ea380515e8b827c953dfa8
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-06 12:42:53 +0300

    edit secure.sh

--- secure.sh
+++ secure.sh
@@ -74,6 +74,7 @@ net.ipv4.tcp_dsack=0
 net.ipv4.tcp_sack=0
 net.ipv4.tcp_workaround_signed_windows=1
 net.ipv4.icmp_echo_ignore_all=1" | sudo tee -a /etc/sysctl.d/99-swappiness.conf
+sudo sysctl -p --system
 
 #--PREVENT IP SPOOFS
 echo -e "order bind,hosts
