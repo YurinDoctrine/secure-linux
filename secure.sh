1b1954571ca2e7f50eaad2c08cf003c15328e58b
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-04-18 09:27:17 +0300

    edit secure.sh

--- secure.sh
+++ secure.sh
@@ -22,11 +22,11 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-#--Required Packages: ufw fail2ban net-tools certbot iwd gnutls
+#--Required Packages: openssl earlyoom ufw fail2ban net-tools certbot iwd gnutls
 which apt >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     sudo apt update &&
-        sudo apt install -f --assume-yes --install-recommends openssl ufw fail2ban gnome-keyring libsecret-1-0 libpam-gnome-keyring net-tools unattended-upgrades proxychains ca-certificates certbot anacron seccomp cryptsetup iwd gnutls-bin libpipeline-dev gpm xfonts-terminus &&
+        sudo apt install -f --assume-yes --install-recommends openssl earlyoom ufw fail2ban gnome-keyring libsecret-1-0 libpam-gnome-keyring net-tools unattended-upgrades proxychains ca-certificates certbot anacron seccomp cryptsetup iwd gnutls-bin libpipeline-dev gpm xfonts-terminus &&
         sudo apt install -f --assume-yes --install-recommends doas
     echo -e 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee /etc/apt/apt.conf.d/50unattended-upgrades
     echo -e 'APT::Periodic::AutocleanInterval "7";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
@@ -40,12 +40,12 @@ fi
 which pacman >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     sudo pacman -Syy &&
-        yay -S --needed --noconfirm openssl ufw fail2ban gnome-keyring libsecret libgnome-keyring net-tools proxychains ca-certificates certbot doas cronie libseccomp cryptsetup iwd gnutls libpipeline gpm terminus-font
+        yay -S --needed --noconfirm openssl earlyoom ufw fail2ban gnome-keyring libsecret libgnome-keyring net-tools proxychains ca-certificates certbot doas cronie libseccomp cryptsetup iwd gnutls libpipeline gpm terminus-font
 fi
 which dnf >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     sudo dnf check-update
-    sudo dnf install openssl ufw fail2ban gnome-keyring libsecret libgnome-keyring net-tools proxychains-ng ca-certificates certbot doas cronie libseccomp cryptsetup iwd gnutls libpipeline gpm terminus-fonts -y
+    sudo dnf install openssl earlyoom ufw fail2ban gnome-keyring libsecret libgnome-keyring net-tools proxychains-ng ca-certificates certbot doas cronie libseccomp cryptsetup iwd gnutls libpipeline gpm terminus-fonts -y
 fi
 
 clear
