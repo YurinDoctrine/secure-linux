3f25bcae84fa47f9f3951d6c4ae4bdbf6bde2777
Yurin Doctrine <doctriney@protonmail.com>
2022-12-08 10:46:19 +0300

    edit secure.sh

--- secure.sh
+++ secure.sh
@@ -94,7 +94,7 @@ net.ipv4.icmp_echo_ignore_all=1
 net.ipv4.route.flush=1
 net.ipv4.ipfrag_time=0
 net.core.default_qdisc=fq_pie
-net.core.busy_poll=0" | sudo tee -a /etc/sysctl.d/99-swappiness.conf
+net.core.busy_read=50" | sudo tee -a /etc/sysctl.d/99-swappiness.conf
 sudo sysctl -p --system
 
 #--PREVENT IP SPOOFS
