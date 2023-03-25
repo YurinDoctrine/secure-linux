a410daebb775865ed5ddb037f81b13cbfc5153b3
Yurin Doctrine <doctriney@protonmail.com>
2023-03-25 23:31:52 +0300

    edit secure.sh

--- secure.sh
+++ secure.sh
@@ -122,8 +122,8 @@ net.core.busy_read=50
 net.core.high_order_alloc_disable=0
 net.core.warnings=0
 net.core.tstamp_allow_data=1
-net.core.enable_tcp_offloading=1
-net.core.netdev_tstamp_prequeue=1" | sudo tee -a /etc/sysctl.d/99-swappiness.conf
+net.core.netdev_tstamp_prequeue=1
+net.core.enable_tcp_offloading=1" | sudo tee -a /etc/sysctl.d/99-swappiness.conf
 sudo sysctl -p --system
 
 #--PREVENT IP SPOOFS
