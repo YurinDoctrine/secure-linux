cd86f04247d2e9ffde91ef87fc972c1f38e6e6bf
Yurin Doctrine <doctriney@protonmail.com>
2023-06-03 00:03:21 +0300

    edit secure.sh

--- secure.sh
+++ secure.sh
@@ -27,7 +27,7 @@ which apt >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     sudo apt update &&
         sudo apt install -f --assume-yes --install-recommends doas
-    sudo apt install -f --assume-yes --install-recommends openssl earlyoom ufw fail2ban gnome-keyring libsecret-1-0 libpam-gnome-keyring net-tools unattended-upgrades proxychains ca-certificates certbot anacron cryptsetup iwd gnutls-bin libpipeline-dev gpm xfonts-terminus usbguard clamav firejail lynis
+    sudo apt install -f --assume-yes --install-recommends openssl earlyoom ufw fail2ban libsecret-1-dev net-tools proxychains ca-certificates certbot anacron cryptsetup iwd gnutls-bin libpipeline-dev firejail lynis
     echo -e 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee /etc/apt/apt.conf.d/50unattended-upgrades
     echo -e 'APT::Periodic::AutocleanInterval "7";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
     echo -e 'APT::Periodic::Download-Upgradeable-Packages "1";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
@@ -35,17 +35,16 @@ if [ $? -eq 0 ]; then
     echo -e 'Unattended-Upgrade::Remove-Unused-Dependencies "true";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
     echo -e 'Unattended-Upgrade::AutoFixInterruptedDpkg "true";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
     echo -e 'Unattended-Upgrade::MinimalSteps "true";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
-    sudo dpkg-reconfigure -f noninteractive unattended-upgrades
 fi
 which pacman >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     sudo pacman -Syy &&
-        yay -S --needed --noconfirm openssl earlyoom ufw fail2ban gnome-keyring libsecret libgnome-keyring net-tools proxychains ca-certificates certbot doas cronie cryptsetup iwd gnutls libpipeline gpm terminus-font usbguard clamav firejail lynis
+        yay -S --needed --noconfirm openssl earlyoom ufw fail2ban libsecret net-tools proxychains ca-certificates certbot doas cronie cryptsetup iwd gnutls libpipeline firejail lynis
 fi
 which dnf >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     sudo dnf check-update
-    sudo dnf install openssl earlyoom ufw fail2ban gnome-keyring libsecret libgnome-keyring net-tools proxychains-ng ca-certificates certbot doas cronie cryptsetup iwd gnutls libpipeline gpm terminus-fonts usbguard clamav firejail lynis -y
+    sudo dnf install openssl earlyoom ufw fail2ban libsecret net-tools proxychains-ng ca-certificates certbot doas cronie cryptsetup iwd gnutls libpipeline firejail lynis -y
 fi
 
 #--Update firmware
