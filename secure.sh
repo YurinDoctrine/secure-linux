77e1bc3f041c9661aa01e58c8e57ef98a078e6a5
Yurin Doctrine <doctriney@protonmail.com>
2023-07-07 19:40:22 +0300

    edit secure.sh

--- secure.sh
+++ secure.sh
@@ -284,6 +284,8 @@ sudo e2fsck -f /dev/sd*[!0-9]
 sudo e2fsck -f /dev/mmcblk*
 sudo e2fsck -f /dev/nvme*
 
+#--Disable add random
+echo -e 'ACTION=="add|change", KERNEL=="sd*[!0-9]|sr*|mmcblk[0-9]*|nvme[0-9]*", ATTR{queue/add_random}=="0"' | sudo tee /etc/udev/rules.d/10-add-random.rules
 #--Disable I/O stats
 echo -e 'ACTION=="add|change", KERNEL=="sd*[!0-9]|sr*|mmcblk[0-9]*|nvme[0-9]*", ATTR{queue/iostats}="0"' | sudo tee /etc/udev/rules.d/20-iostats.rules
 #--Optimize disk read-ahead
